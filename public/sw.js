ab2c23f68f2745dd708020cfdeab152624923fdb
CANU <canu1832@gmail.com>
2025-12-05 00:27:13 +0900

    fix: push 알림 플랫폼 최적화

--- public/sw.js
+++ public/sw.js
@@ -3,13 +3,9 @@ self.addEventListener('push', function (event) {
         const data = event.data.json();
         const options = {
             body: data.body,
-            icon: data.icon || '/icon-192x192.png',
-            badge: '/badge.png',
-            vibrate: [100, 50, 100],
-            data: {
-                dateOfArrival: Date.now(),
-                primaryKey: '2'
-            }
+            icon: '/icon-192x192.png',
+            tag: 'hanavi-schedule-update', // Group notifications
+            renotify: true // Alert even if same tag
         };
         event.waitUntil(self.registration.showNotification(data.title, options));
     }
